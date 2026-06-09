69868773b8fb31633deba60ba9e88049c67b86d0
shivampratapsi <shivampratapsingh181@gmail.com>
2026-06-09 23:12:06 +0530

    [Shivam Pratap Singh] Fixed variable issue.

--- src/main/java/com/fundoo/fundoonotes/dto/request/NoteReqDTO.java
+++ src/main/java/com/fundoo/fundoonotes/dto/request/NoteReqDTO.java
@@ -1,17 +1,15 @@
 package com.fundoo.fundoonotes.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class NoteReqDTO {
 
-    // @NotBlank(message = "Title is must")
+
     @Size(max = 50, message = "Title cannot exceed 50 characters")
     private String title;
 
-    // @NotBlank(message = "Description is must for note")
     @Size(max = 255, message = "Description is too long (maximum 255 characters)")
     private String description;
 
